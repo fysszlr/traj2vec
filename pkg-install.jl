e849bc29825f4e15c0541f0b872c6930d2693c68
fysszlr <fysszlr@qq.com>
2023-12-21 00:05:43 +0800

    init

---
+++ pkg-install.jl
@@ -0,0 +1,12 @@
+using Pkg
+
+Pkg.add("StatsBase")
+Pkg.add("HDF5")
+Pkg.add("DataStructures")
+Pkg.add("NearestNeighbors")
+Pkg.add("JSON")
+Pkg.add("Serialization")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+Pkg.add("Distances")
+Pkg.add("IJulia")
